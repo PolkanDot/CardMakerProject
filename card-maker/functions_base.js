c5e2e467f7a08704309d57ccaaceb428098f14e5
PolkanDot <insarmiftahov@yandex.ru>
2021-11-09 21:58:28 +0300

    Create and start work in functions_base.js

---
+++ card-maker/functions_base.js
@@ -0,0 +1,16 @@
+/**
+ * @returns {CardMaker}
+ */
+function createNewCard() { }
+
+/**
+ * @returns {CardMaker}
+ */
+function DeleteCard() { }
+
+/**
+ * @param {Patherns} patherns
+ * @param {string} pathernName
+ * @returns {CardMaker}
+ */
+function usePathern(patherns, pathernName) {}
