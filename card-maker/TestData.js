f0b36804d01f5bdd2c460b8d3038f4e06fc5f547
PolkanDot <insarmiftahov@yandex.ru>
2021-11-20 20:06:49 +0300

    Add TextObject and ArtObject

--- card-maker/TestData.js
+++ card-maker/TestData.js
@@ -8,9 +8,29 @@ const FieldCoordinates = {
   y: 0
 };
 
+const ArtCoordinates = {
+  x: 700,
+  y: 300
+};
+
+const TextCoordinates = {
+  x: 500,
+  y: 100,
+};
+
 const ImgSize = {
   Height : 50,
   Width : 50
+};
+
+const TextSize = {
+  Height: 30,
+  Width: 100
+};
+
+const ArtSize = {
+  Height: 100,
+  Width: 100
 };
 
 const FieldSize = {
@@ -19,36 +39,66 @@ const FieldSize = {
 };
 
 const CardSize = {
-  Height = 1500,
-  Width = 1000
-}
+  Height: 1500,
+  Width: 1000
+};
 
-const SelectedElements = [];
+const SelectedElement = [];
 
-const States = [Card]
+const ImageElement = {
+  Coordinates: ImgCoordinates,
+  Size: ImgSize,
+  ElementId: 0,
+  ImgData: "/9j/4AAQSkZJRgABAQEBLAEsAAD / 2wBDAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsKCwsNDhIQDQ4RDgrBZf//Z"
+};
 
-const ImageObj = {
-  Coordinates = Coordinates,
-  Size = Size,
-  ElementId = 0,
-  ImgData = "/9j/4AAQSkZJRgABAQEBLAEsAAD / 2wBDAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsKCwsNDhIQDQ4RDgrBZf//Z"
+const TextElement = {
+  Coordinates: TextCoordinates,
+  Size: TextSize,
+  ElementId: 1,
+  TextData: "Hello World!",
+  TextSize: 20,
+  Font: "Arial",
+  Italic: False,
+  FontThickness: 0,
+  Color: "000000",
+  Background: null
+};
+
+const ArtElement = {
+  Coordinates: ArtCoordinates,
+  Size: ArtSize,
+  ElementId: 2,
+  ArtData: "wBDAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsKCwsNDhIQDQ4RDgrBZf"
 };
 
 const Field = {
-  Size = FieldSize,
-  Coordinates = FieldCoordinates
+  Size: FieldSize,
+  Coordinates: FieldCoordinates
 };
 
 const Background = {
-  Color = 'FFFFFF',
-  ImageData = Nil
+  Color: 'FFFFFF',
+  ImageData: null
 };
 
 const Card = {
-  DisplayList = [0],
-  Objects = [ImageObj],
-  Field = Field,
-  Background = Background,
-  Size = CardSize,
-  CardName = 'New card'
+  DisplayList: [1, 0, 2],
+  Objects: [ImageElement, TextElement, ArtElement],
+  Field: Field,
+  Background: Background,
+  Size: CardSize,
+  CardName: 'New card'
+};
+
+const MakerHistory = {
+  States: [Card],
+  Index: 0
+};
+
+const CardMaker = {
+  SelectedElements: SelectedElements,
+  Patherns: [],
+  Card: Card,
+  MakerHisory: MakerHistory 
 };
