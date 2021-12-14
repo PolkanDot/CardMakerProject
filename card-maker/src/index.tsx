e10e11b69892864c854dcfbe5f666ffe72947389
PolkanDot <insarmiftahov@yandex.ru>
2021-12-14 14:27:27 +0300

    Rename index.js into index.tsx

---
+++ card-maker/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './CardMaker/CardMaker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
