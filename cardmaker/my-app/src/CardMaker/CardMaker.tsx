f8b49cbbd07087eba6e8c2745114507d2c9d8dc8
PolkanDot <insarmiftahov@yandex.ru>
2021-12-28 10:46:39 +0300

    Replace seperate creating NavBar and ToolBar to common

--- cardmaker/my-app/src/CardMaker/CardMaker.tsx
+++ cardmaker/my-app/src/CardMaker/CardMaker.tsx
@@ -1,6 +1,9 @@
 import style from './CardMaker.module.css';
 import FunCard from './Card/Card';
+import FunNavBar from './TopMenu/NavBar/NavBar';
+import FunToolBar from './TopMenu/ToolBar/ToolBar';
 import { CardMaker } from '../Types';
+import FunTopMenu from './TopMenu/TopMenu';
 
 type CardMakerProps = {
     cardMaker: CardMaker
@@ -9,6 +12,7 @@ type CardMakerProps = {
 export default function FunCardMaker(props: CardMakerProps) {
     return (
         <div className={style.cardMaker}>
+            <FunTopMenu />
             <FunCard card={props.cardMaker.card} />
         </div>
     );
